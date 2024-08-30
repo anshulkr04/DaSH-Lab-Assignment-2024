f056f0723199299dc0c6cea8be9cd34f9e763f8f
AnshulKumar <154264619+anshulkr04@users.noreply.github.com>
2024-08-30 10:36:22 +0530

    Create script.sh

---
+++ Development/Level-2/script.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+cd ..
+cd ..
+source .venv/bin/activate
+cd Development/Level-2
+fastapi dev
+# Define the directory path and script name
+DIR_PATH="Desktop/DD/DaSH-Lab-Assignment-2024/Development/Level-2"
+SCRIPT_NAME="client.py"
+
+# Function to open a new terminal, change directory, and run the Python script
+run_client() {
+    osascript <<EOF
+    tell application "Terminal"
+        do script "cd Desktop;cd DaSH-Lab-Assignment-2024;source .venv/bin/activate;cd Development;cd Level-2; python3 '$SCRIPT_NAME'"
+    end tell
+EOF
+}
+
+# Run the command three times in new terminal windows with a 1-second delay between each
+for i in {1..3}
+do
+    run_client
+    sleep 1
+done
